1309b0398f0651a2029faa13dea25342c022d616
khsyukatu <higuchi@unistyleinc.com>
2015-09-08 04:19:36 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,20 @@ class User < ActiveRecord::Base
     has_secure_password
     
     has_many :microposts
+    has_many :following_relationships, class_name: "Relationship",
+                                       foreign_key: "follower_id",
+                                       dependent: :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+  
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
