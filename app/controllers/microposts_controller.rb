1b83780103ed8a69d5fa30165b1ca98eb1bb9e67
khsyukatu <higuchi@unistyleinc.com>
2015-09-10 01:25:36 +0000

    retweet function modified

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create]
     
     def create
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page( params[:page]).per(1)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page( params[:page]).per(10)
       @micropost = current_user.microposts.build(micropost_params)
       if @micropost.save
         flash[:success] = "Micropost created!"
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,12 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  has_many :retweets, :class_name => "Micropost", :foreign_key => "retweet_from_id"
   belongs_to :retweet_from, class_name: "Micropost"
+  has_many :retweets, :class_name => "Micropost", :foreign_key => "retweet_from_id"
+  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }, if: "retweet_from_id.nil?"
+  validates_uniqueness_of :retweet_from_id, :scope => :user_id
   
   mount_uploader :image, ImageUploader
-
+  
 end
